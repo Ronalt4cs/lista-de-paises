9c13f24b3937f89885a3ed20e12920497cb2dac7
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-05-20 23:37:14 -0300

    layout e listagem de paises

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    // https://flagcdn.com/w320/kw.png
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'flagcdn.com',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
